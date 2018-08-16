2c61b3a90681745e0a139992ca497b1df1f971e7
Jatin Kumar <jatn15011999@gmail.com>
2018-08-16 22:56:02 +0530

    Initial Design

--- app.js
+++ app.js
@@ -8,7 +8,27 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.get('/', function (req, res) {
-    res.render("index");
+    res.render("pages/index");
+});
+
+app.get('/projects', function (req, res) {
+    res.render("pages/projects");
+});
+
+app.get('/team', function (req, res) {
+    res.render("pages/team");
+});
+
+app.get('/join', function (req, res) {
+    res.render("pages/join");
+});
+
+app.get('/blog', function (req, res) {
+    res.render("pages/blog");
+});
+
+app.get('/media', function (req, res) {
+    res.render("pages/media");
 });
 
 app.listen(process.env.PORT || 8080, function(){
