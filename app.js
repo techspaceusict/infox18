aa3d7fa779e98cc6cced7834a012b95a705256ba
Shubham Gupta <sg270798@gmail.com>
2018-08-19 23:37:01 +0530

    backend started

--- app.js
+++ app.js
@@ -1,14 +1,74 @@
-var express = require("express"),
+const express = require("express"),
     app = express(),
-    bodyParser = require("body-parser");
+    bodyParser = require("body-parser"),
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    flash = require('connect-flash'),
+    morgan = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    configDB = require('./config/db'),
+    Users = require('./models/user');
+
+mongoose.connect(configDB.url, {useNewUrlParser: true})
+    .then(function () {
+        console.log("mongo connected!");
+    })
+    .catch(function (err) {
+        console.log('err:' + err);
+    });
+require('./config/passport')(passport);
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(session({
+    secret: 'infox123',
+    saveUninitialized: true,
+    resave: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated() === true){
+        return next();
+    }
+    res.redirect('/signIn');
+}
 
 app.get('/', function (req, res) {
     res.render("pages/index");
+});
+
+app.get('/signIn', function (req,res) {
+    res.render("pages/signIn", {message: req.flash("signUpLogInMessage")});
+});
+
+app.post('/signUp', passport.authenticate('local-signUp', {
+    successRedirect: '/',
+    failureRedirect: '/signIn',
+    failureFlash: true
+}));
+
+app.post('/signIn', passport.authenticate('local-signIn', {
+    successRedirect: '/profile',
+    failureRedirect: '/signIn',
+    failureFlash: true
+}));
+
+app.get('/profile', isLoggedIn, function (req, res) {
+    console.log(req.user);
+    res.render('pages/profile', {user: req.user});
+});
+
+app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
 });
 
 app.get('/projects', function (req, res) {
