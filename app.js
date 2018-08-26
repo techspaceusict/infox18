6d13d1642cbe58b52a4d5b947ca6c375072bbcb7
Jatin Kumar <jatin15011999@gmail.com>
2018-08-26 21:28:31 +0530

    changes fetched

--- app.js
+++ app.js
@@ -62,21 +62,21 @@ app.post('/signIn', passport.authenticate('local-signIn', {
     failureFlash: true
 }));
 
-// app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email'] }));
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email'] }));
 
 // Facebook will redirect the user to this URL after approval.  Finish the
 // authentication process by attempting to obtain an access token.  If
 // access was granted, the user will be logged in.  Otherwise,
 // authentication has failed.
-// app.get('/auth/facebook/callback',
+app.get('/auth/facebook/callback',
     passport.authenticate('facebook', { successRedirect: '/profile',
         failureRedirect: '/signIn' }));
 
-// app.get('/auth/google', passport.authenticate('google', { scope: ['email'] }));
+app.get('/auth/google', passport.authenticate('google', { scope: ['email'] }));
 
-// app.get('/auth/google/callback',
-//     passport.authenticate('google', { successRedirect: '/profile',
-//         failureRedirect: '/signIn' }));
+app.get('/auth/google/callback',
+    passport.authenticate('google', { successRedirect: '/profile',
+        failureRedirect: '/signIn' }));
 
 app.get('/profile', isLoggedIn, function (req, res) {
     console.log(req.user);
