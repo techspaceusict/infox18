13c61ef2bd3d3fc22010692d51855312af5f52bd
ashish0910 <ashish09101998@gmail.com>
2018-09-02 11:12:50 +0530

    contact form complete

---
+++ public/js/contact.js
@@ -0,0 +1,101 @@
+var NameInput = React.createClass({displayName: "NameInput",
+    handleTextChange: function(){
+        var x = this.refs.nameField.getDOMNode().value;
+        
+        if(x != ''){
+            this.refs.nameField.getDOMNode().className = 'active';
+        } else {
+            this.refs.nameField.getDOMNode().className = '';
+        }
+
+        this.props.onUserInput(x);
+    },
+    render: function(){
+        return (
+            React.createElement("div", {className: "control"}, 
+                React.createElement("input", {type: "text", id: "name", ref: "nameField", placeholder: "Please Enter your Name", autoFocus: true, required: true, onChange: this.handleTextChange}), 
+                React.createElement("label", {for: "name"}, "Name")
+            )
+        )
+    }
+});
+
+var EmailInput = React.createClass({displayName: "EmailInput",
+    handleTextChange: function(){
+        var x = this.refs.emailField.getDOMNode().value;
+        
+        if(x != ''){
+            this.refs.emailField.getDOMNode().className = 'active';
+        } else {
+            this.refs.emailField.getDOMNode().className = '';
+        }
+
+        this.props.onUserInput('', x);
+    },
+    render: function(){
+        return (
+            React.createElement("div", {className: "control"}, 
+                React.createElement("input", {type: "email", id: "email", ref: "emailField", placeholder: "Please Enter your email", required: true, onChange: this.handleTextChange}), 
+                React.createElement("label", {for: "email"}, "e-mail")
+            )
+        )
+    }
+});
+
+var MessageArea = React.createClass({displayName: "MessageArea",
+    handleTextChange: function(){
+        var x = this.refs.messageBox.getDOMNode().value;
+        
+        if(x != ''){
+            this.refs.messageBox.getDOMNode().className = 'active';
+        } else {
+            this.refs.messageBox.getDOMNode().className = '';
+        }
+
+        this.props.onUserInput('', '', x);
+    },
+    render: function(){
+        return (
+            React.createElement("div", {className: "control"}, 
+                React.createElement("textarea", {id: "message", ref: "messageBox", placeholder: "What's on your mind?", required: true, onChange: this.handleTextChange}), 
+                React.createElement("label", {for: "message"}, "Message")
+            )
+        )
+    }
+});
+
+var ContactForm = React.createClass({displayName: "ContactForm",
+    getInitialState: function() {
+        return {
+            nameText: '',
+            emailText: '',
+            messageText: ''
+        };
+    },
+    handleUserInput: function(nameText, emailText, messageText) {
+        this.setState({
+            nameText: nameText,
+            emailText: emailText,
+            messageText: messageText
+        });
+    },
+  render: function(){
+    return (
+         React.createElement("form", {action: "/"}, 
+        
+            React.createElement("fieldset", null, 
+                React.createElement("legend", null, "Contact me."), 
+                
+                React.createElement(NameInput, {onUserInput: this.handleUserInput}), 
+                React.createElement(EmailInput, {onUserInput: this.handleUserInput}), 
+                React.createElement(MessageArea, {onUserInput: this.handleUserInput}), 
+                
+                React.createElement("input", {type: "submit", value: "send"})
+            )
+
+        )
+        );
+  }
+});
+
+React.render(React.createElement(ContactForm, null), document.getElementById('stage'));
