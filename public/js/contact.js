9c72bd06628865d275d27138ac10b4989a756ecf
Jatin Kumar <jatin15011999@gmail.com>
2018-10-02 17:59:37 +0530

    contact form + response saved animation

--- public/js/contact.js
+++ public/js/contact.js
@@ -102,4 +102,81 @@
 
 // alert('kakak');
 
+// Auto resize input
+function resizeInput() {
+    $(this).attr('size', $(this).val().length);
+}
+
+$('input[type="text"], input[type="email"]')
+    // event handler
+    .keyup(resizeInput)
+    // resize on page load
+    .each(resizeInput);
+
+
+console.clear();
+// Adapted from georgepapadakis.me/demo/expanding-textarea.html
+(function(){
   
+  var textareas = document.querySelectorAll('.expanding'),
+      
+      resize = function(t) {
+        t.style.height = 'auto';
+        t.style.overflow = 'hidden'; // Ensure scrollbar doesn't interfere with the true height of the text.
+        t.style.height = (t.scrollHeight + t.offset ) + 'px';
+        t.style.overflow = '';
+      },
+      
+      attachResize = function(t) {
+        if ( t ) {
+          console.log('t.className',t.className);
+          t.offset = !window.opera ? (t.offsetHeight - t.clientHeight) : (t.offsetHeight + parseInt(window.getComputedStyle(t, null).getPropertyValue('border-top-width')));
+
+          resize(t);
+
+          if ( t.addEventListener ) {
+            t.addEventListener('input', function() { resize(t); });
+            t.addEventListener('mouseup', function() { resize(t); }); // set height after user resize
+          }
+
+          t['attachEvent'] && t.attachEvent('onkeyup', function() { resize(t); });
+        }
+      };
+  
+  // IE7 support
+  if ( !document.querySelectorAll ) {
+  
+    function getElementsByClass(searchClass,node,tag) {
+      var classElements = new Array();
+      node = node || document;
+      tag = tag || '*';
+      var els = node.getElementsByTagName(tag);
+      var elsLen = els.length;
+      var pattern = new RegExp("(^|\\s)"+searchClass+"(\\s|$)");
+      for (i = 0, j = 0; i < elsLen; i++) {
+        if ( pattern.test(els[i].className) ) {
+          classElements[j] = els[i];
+          j++;
+        }
+      }
+      return classElements;
+    }
+    
+    textareas = getElementsByClass('expanding');
+  }
+  
+  for (var i = 0; i < textareas.length; i++ ) {
+    attachResize(textareas[i]);
+  }
+})();
+
+$('#send-icon').click(function(){
+    $(".contact-sub-wrapper").addClass('animated-contact-sub-wrapper').promise().done(function(){
+        $(".contact-sub-wrapper").css({display:'none'});
+    });
+    $('.res-saved').addClass('res-animated')
+    // setTimeout(function(){
+    //     $(".contact-sub-wrapper").css({display:'none'});
+    //     $(".res-saved").css({dispaly:'flex'});
+    // },500)
+});
